c6ad4ecca999004f39dcfe9d16a4a8bf82f5ac70
Jun Shao <jshao@futurewei.com>
2021-11-22 21:25:35 +0000

    Did a duplicate vni check

--- mizar/dp/mizar/operators/vpcs/vpcs_operator.py
+++ mizar/dp/mizar/operators/vpcs/vpcs_operator.py
@@ -50,13 +50,14 @@ class VpcOperator(object):
 
     def query_existing_vpcs(self):
         def list_vpc_obj_fn(name, spec, plurals):
-            logger.info("Bootstrapped {}".format(name))
+            #logger.info("Bootstrapped {} with vni {} with status {}".format(name, v.vni, v.status))
             v = self.get_vpc_stored_obj(name, spec)
+            logger.info("Bootstrapped {} with vni {} with status {}".format(name, v.vni, v.status))
             if v.status == OBJ_STATUS.vpc_status_provisioned:
                 self.store_update(v)
 
         kube_list_obj(self.obj_api, RESOURCES.vpcs, list_vpc_obj_fn)
-        logger.debug("Bootstrap VPC store: {}".format(self.store._dump_vpcs()))
+        logger.info("Bootstrap VPC store: {}".format(self.store._dump_vpcs()))
 
     def get_vpc_tmp_obj(self, name, spec):
         return Vpc(name, self.obj_api, None, spec)
@@ -124,6 +125,12 @@ class VpcOperator(object):
         logger.info("The current vpc is {} and the vni is {}".format(vpc.name, vpc.vni))
         if vpc.vni is None:
             vpc.set_vni(str(uuid.uuid4().int & (1 << 24)-1))
+        else:
+            stored_vpcs = self.obj_api.list_cluster_custom_object(group="mizar.com", version="v1", plural="vpcs")
+            for stored_vpc in stored_vpcs["items"]:
+                if stored_vpc["spec"]["vni"] == vpc.vni and stored_vpc["metadata"]["name"] != vpc.name:
+                    vpc.set_vni(str(uuid.uuid4().int & (1 << 24)-1))
+                    logger.info("There is a duplicate vni {} and it has been changed to a new one.".format(stored_vpc["spec"]["vni"], vpc.vni))
         logger.info("The updated vni is {}".format(vpc.vni))
     
     def deallocate_vni(self, vpc):
